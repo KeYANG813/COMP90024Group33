1bbb68c30cba551b1b0b837e9cac963b5fb27276
ccc <ccc@ravpn-200-student-ten-8-21-226.uniaccess.unimelb.edu.au>
2022-04-24 22:51:25 +0800

    clone git to ubuntu

--- ansible_new/run_crawler_cloud.sh
+++ ansible_new/run_crawler_cloud.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-. ./openrc.sh; ansible-playbook -i hosts run_crawler_cloud.yaml
+. ./openrc.sh; ansible-playbook -i hosts --ask-become-pass run_crawler_cloud.yaml --check -vvv
 # . ./openrc.sh; ansible-playbook -i hosts -u ubuntu --key-file=~/.ssh/test.pem run_crawler_cloud.yaml
