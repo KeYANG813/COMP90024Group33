4c767a5bcdbaba1ed49848d1851732c9f356d549
KeYANG813 <yk1148910416@163.com>
2022-04-24 15:42:38 +0800

    update account

--- simon/ccc_v2/code/tweet_4_18_v2.py
+++ simon/ccc_v2/code/tweet_4_18_v2.py
@@ -25,17 +25,17 @@ def account_info():
                 "Access_Token_Secret" : "94Do2on02xCemCwpfg9PyCikFswDsAcufuYQTisJTrtWh"
                 },
             "christina": {
-                "consumer_key": "5KHYDpEavEvWs6f07gqA7FSYU",
-                "consumer_secret": "15T8ePaIu7IfU0X7VnhPHAsgSjvIqpsYTpDEKjI596w6mUoTLe",
-                "access_token": "1517696674243948545-ozARJ8xRdGarhfknxGHqvMs51uPXp3",
-                "access_token_secret": "rN3JxUTER0PjMXqsy4WSgzqGk10IF29IBG7JXSotRUAxG",
+                "Consumer_Key": "5KHYDpEavEvWs6f07gqA7FSYU",
+                "Consumer_Secret": "15T8ePaIu7IfU0X7VnhPHAsgSjvIqpsYTpDEKjI596w6mUoTLe",
+                "Access_Token": "1517696674243948545-ozARJ8xRdGarhfknxGHqvMs51uPXp3",
+                "Access_Token_Secret": "rN3JxUTER0PjMXqsy4WSgzqGk10IF29IBG7JXSotRUAxG",
                 "bearer_token":"AAAAAAAAAAAAAAAAAAAAAEo9bwEAAAAA40DLUb109523Heu6ePJD3vtThCc%3DjIH9qTJi0qCyhH9V98DtTQLLMmoV9V2mSRr4W5f9tF8m8Iee8y"
             },
             "wendy": {
-                "consumer_key": "mPbAIQI6GiUKsR89tBZ1dOVte",
-                "consumer_secret": "0Vsm4cXRuLutFREUj0elbpF92PyyIb29TSR1BdwGXivLUftVD9",
-                "access_token": "1510827213339623431-U1mlt70yY2bAd9v7WHlYGpTg3RCUJK",
-                "access_token_secret": "gftHoztKdJ5em60kms3aqqRIsswcAGLRZDPYGbf6ESf1X",
+                "Consumer_Key": "mPbAIQI6GiUKsR89tBZ1dOVte",
+                "Consumer_Secret": "0Vsm4cXRuLutFREUj0elbpF92PyyIb29TSR1BdwGXivLUftVD9",
+                "Access_Token": "1510827213339623431-U1mlt70yY2bAd9v7WHlYGpTg3RCUJK",
+                "Access_Token_Secret": "gftHoztKdJ5em60kms3aqqRIsswcAGLRZDPYGbf6ESf1X",
                 "bearer_token":"AAAAAAAAAAAAAAAAAAAAAJZqbAEAAAAAiiSxVh7G3TjQIB0UajmQrZJWPIE%3DibLwxyjeQOyLRYps5puQq4jtgPDkrz9tM9uDUAtx3pwurWerON"
             }
         },
@@ -45,7 +45,6 @@ def account_info():
             "url": "http://127.0.0.1:5984"
         }
     }
-
     return account
 
 
@@ -56,7 +55,7 @@ class IDPrinter(tweepy.Stream):
         if not tweetJson["text"].startswith('RT') and tweetJson["retweeted"] == False:
             db.create_document(tweetJson)
             print("get")
-            print(newJSON)
+            # print(newJSON)
     
     def on_status(self, status):
         print(status.id)
@@ -64,10 +63,10 @@ class IDPrinter(tweepy.Stream):
 
 if __name__ == '__main__':
     account = account_info()
-    Consumer_Key = account["accounts"]["xwqian2"]["Consumer_Key"]
-    Consumer_Secret = account["accounts"]["xwqian2"]["Consumer_Secret"]
-    Access_Token = account["accounts"]["xwqian2"]["Access_Token"]
-    Access_Token_Secret = account["accounts"]["xwqian2"]["Access_Token_Secret"]
+    Consumer_Key = account["accounts"]["christina"]["Consumer_Key"]
+    Consumer_Secret = account["accounts"]["christina"]["Consumer_Secret"]
+    Access_Token = account["accounts"]["christina"]["Access_Token"]
+    Access_Token_Secret = account["accounts"]["christina"]["Access_Token_Secret"]
     stream = tweepy.Stream(
     Consumer_Key, Consumer_Secret,
     Access_Token, Access_Token_Secret
