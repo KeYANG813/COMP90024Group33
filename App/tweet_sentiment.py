f50ebc41ff75a7062e82e056e46da3083e862b2d
KeYANG813 <yk1148910416@163.com>
2022-05-13 23:47:44 +0800

    live_tweet_sentiment

--- App/tweet_sentiment.py
+++ App/tweet_sentiment.py
@@ -22,6 +22,7 @@ import nltk
 nltk.download('vader_lexicon')
 from nltk.sentiment import SentimentIntensityAnalyzer
 import re
+import os
 
 
 def textProcess(text):
@@ -62,6 +63,9 @@ def tweet_analysis():
     }
     '''
     client = couchdb_init()
+
+    allCityResults = {}
+    nums = []
     for city in dbname:
         citydb = client[city]
         json_data = json.loads(design_hashtag_doc, strict=False)
@@ -79,10 +83,12 @@ def tweet_analysis():
         create_view = View(citydb['_design/text'], 'new_view')
 
         with create_view.custom_result(group=True) as results:
-            i = 0
+            index = 0
             for result in results:
-                text_dic[i]= result['key']
-                i += 1
+                text_dic[index]= result['key']
+                index += 1
+
+        nums.append(index)
                 
         c_score_sum = 0
         neg_score_sum = 0
@@ -107,13 +113,56 @@ def tweet_analysis():
         ave_pos_score = pos_score_sum/len(text_dic.values()) 
         pos_score.append(ave_pos_score)
 
+
     results = {} 
     results["compound"] = compound_score
     results["neg"] = neg_score
     results["neu"] = neu_score
     results["pos"] = pos_score
-    
-    return results
+
+    save_results = results
+    save_results["nums"] = nums
+
+    # save_results:
+    # {"compound":[xx,xx,xx,xx,xx],
+    #  "neg":[xx,xx,xx,xx,xx],
+    #   ...
+    #  "nums":[xx,xx,xx,xx,xx]
+    # }
+
+
+    filePath = "pre_sentiment.txt"
+    if not os.path.exists(filePath):
+        f = open(filePath,"w")
+        try:
+            json.dump(save_results, f)
+        except IOError as e:
+            print("save file error!")
+        finally:
+            f.close()
+
+        return results
+    else:
+        f = open(filePath,"w")
+        try:
+            pre_results = json.load(f)
+
+            for scores in pre_results:
+                if scores != "nums":
+                    for i in pre_results[scores]:
+                        num1 = pre_results["nums"][i]
+                        num2 = save_results["nums"][i]
+                        pre_results[scores][i] = (pre_results[scores][i] * num1 + save_results[scores][i] * num2)/(num1+num2)
+                        pre_results["nums"][i] = num1+num2
+
+            
+            json.dump(pre_results, f)
+        except IOError as e:
+            print("file IO error!")
+        finally:
+            f.close()
+        
+        return pre_results
                 
     
 
