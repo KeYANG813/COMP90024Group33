93b46e011fa352195327551d24f9c6c9e212b2f3
crystal0161 <jintongl1@student.unimelb.edu.au>
2022-05-15 23:56:08 +1000

    remove useless code

--- App/tweet_sentiment.py
+++ App/tweet_sentiment.py
@@ -30,18 +30,10 @@ import requests
 def textProcess(text):
     text = re.sub(r'(https|http)?:\/\/(\w|\.|\/|\?|\=|\&|\%)*\b', " ", text)  # remove URL
     text = text.lower() # lowercase the text
-#         text = re.sub(r'@[^ ]+', ' ', text) # replace mention
-    # text = re.sub(r'#', '', text) # remove hashtag
     text = re.sub(r'([a-z])\1{2,}', r'\1', text) # character normalization
     text = re.sub(r'[^a-z#@0-9 ]', ' ', text) # remove non-letter characters
     text = text.strip()
     return text
-
-# def couchdb_init():
-#     USERNAME = 'user'
-#     PASSWORD = 'pass'
-#     client = CouchDB(USERNAME, PASSWORD, url='http://172.26.132.223:5984', connect=True)
-#     return client
 
 def calculate_city_score(text):
     text = textProcess(text)
@@ -87,25 +79,7 @@ def get_new_tweets(db_name,num_of_tweets):
     return get_sample_tweets(db_name, condition)
 
 def tweet_analysis():
-#     # count hashtags
-#     citys = {}
     dbname = ["db_melbourne", "db_sydney", "db_adelaide", "db_darwin", "db_brisbane"]
-#     dbname = ["db_darwin", "db_sydney", "db_adelaide", "db_darwin", "db_brisbane"]
-#     design_hashtag_doc= '''
-#     {
-#         "_id" : "_design/text",
-#         "language": "javascript",
-#         "views" : {
-#           "new_view" : {
-#             "map": "function(doc){emit([doc.text, doc.created_at], 1)}", 
-#             "reduce" : "_count"}
-#         }
-#     }
-#     '''
-#     client = couchdb_init()
-
-
-    # nums = []
     numCity = {
             "db_melbourne": 5300,
             "db_sydney": 5500,
@@ -119,61 +93,13 @@ def tweet_analysis():
     neu_score = []
     pos_score = []
     for city in dbname:
-        
-        # citydb = client[city]
-        # json_data = json.loads(design_hashtag_doc, strict=False)
-        # client = couchdb_init()
-        
         text_dic = {}
         newTweets = get_new_tweets(city, numCity[city])
         
         index = 0
         for result in newTweets:
             text_dic[index]= result['text']
-            index += 1
-        # if not json_data['_id'] in citydb:
-            
-        #     citydb.create_document(json_data)
-
-        # create_view = View(citydb['_design/text'], 'new_view')
-
-
-        # sevenDayAgo = (datetime.datetime.now() - datetime.timedelta(days = 7))
-        # sevenDayAgoTimeStamp = int(time.mktime(sevenDayAgo.timetuple()))
-
-
-        # with create_view.custom_result(group=True) as results:
-        #     index = 0
-            
-        #     for result in results:                  
-        #         create_time = result['key'][1]
-
-        
-        #         #"Tue Apr 26 14:56:27 +0000 2022",
-        #         items = create_time.split(' ')
-        #         # items =["Tue","Apr",....,"+0000","2022"]
-        #         # 6 
-        #         process_time = ""
-        #         for i in range(len(items)):
-        #             if i!=4:
-        #                 process_time = process_time + items[i] + " "
-        #         length = len(process_time)
-        #         process_time = process_time[0:length-1]
-
-    
-        #         try:
-        #             timestamp = int(time.mktime(time.strptime(process_time,"%a %b %d %H:%M:%S %Y")))
-        #         except:
-        #             print(process_time)
-        #             continue
-     
-                
-                
-        #         if timestamp > sevenDayAgoTimeStamp:
-        #             text_dic[index]= result['key'][0]
-        #             index += 1
-                
-                
+            index += 1          
         c_score_sum = 0
         neg_score_sum = 0
         neu_score_sum = 0
@@ -209,5 +135,5 @@ def tweet_analysis():
     results["polarity"] = polarity
     
     return results
-#print(tweet_analysis())
+
 
